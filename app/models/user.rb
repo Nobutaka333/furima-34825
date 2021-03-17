7dd5135ee66783b1dfb49107ae9809db200853dc
Nobutaka333 <taka9861+techcamp@gmail.com>
2021-03-17 16:34:20 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :last_name, presence: true, format: { 
+           with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を使用してください' }
+         validates :first_name, presence: true, format: { 
+          with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を使用してください' }
+         validates :last_name_kana, presence: true, format: { 
+          with: /\A[ァ-ヶ一]+\z/, message: 'は全角カナを使用してください' }
+         validates :first_name_kana, presence: true, format: { 
+          with: /\A[ァ-ヶ一]+\z/, message: 'は全角カナを使用してください' }
+         validates :user_birthday, presence: true
+         validates :password, format: {
+            with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください"  }
 end
