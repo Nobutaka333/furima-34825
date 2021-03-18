5a61163c15c714e4850b9cb9f5285f505eba6f1f
Nobutaka333 <taka9861+techcamp@gmail.com>
2021-03-18 19:01:16 +0900

    with_optionsを使用したバリデーションに修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: {
-    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を使用してください'
-  }
-  validates :first_name, presence: true, format: {
-    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を使用してください'
-  }
-  validates :last_name_kana, presence: true, format: {
-    with: /\A[ァ-ヶ一]+\z/, message: 'は全角カナを使用してください'
-  }
-  validates :first_name_kana, presence: true, format: {
-    with: /\A[ァ-ヶ一]+\z/, message: 'は全角カナを使用してください'
-  }
-  validates :user_birthday, presence: true
-  validates :password, format: {
-    with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は6文字以上かつ英数字をそれぞれ含めてください'
-  }
+  with_options presence: true do
+    validates :nickname
+    validates :user_birthday
+    with_options format: {
+      with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を使用してください'} do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: {
+      with: /\A[ァ-ヶ一]+\z/, message: 'は全角カナを使用してください'} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は6文字以上かつ英数字をそれぞれ含めてください'}
+  end
 end
