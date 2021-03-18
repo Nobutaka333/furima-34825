f01dbcb773b52af090eb8d171d9ef95a773990c2
Nobutaka333 <taka9861+techcamp@gmail.com>
2021-03-18 18:00:55 +0900

    ログインできなかった問題修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
 
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:nickname, :email, :user_birthday, :first_name, :last_name, :first_name_kana, :last_name_kana]
+      :sign_up, keys: [:nickname,:user_birthday, :first_name, :last_name, :first_name_kana, :last_name_kana]
     )
   end
 
