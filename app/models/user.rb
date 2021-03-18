f8418ddef207d9be8db4c50593269b84e315f2f2
Nobutaka333 <taka9861+techcamp@gmail.com>
2021-03-18 19:06:07 +0900

    rubocopによる記述修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,19 @@ class User < ApplicationRecord
     validates :nickname
     validates :user_birthday
     with_options format: {
-      with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を使用してください'} do
+      with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を使用してください'
+    } do
       validates :last_name
       validates :first_name
     end
     with_options format: {
-      with: /\A[ァ-ヶ一]+\z/, message: 'は全角カナを使用してください'} do
+      with: /\A[ァ-ヶ一]+\z/, message: 'は全角カナを使用してください'
+    } do
       validates :last_name_kana
       validates :first_name_kana
     end
     validates :password, format: {
-    with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は6文字以上かつ英数字をそれぞれ含めてください'}
+      with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は6文字以上かつ英数字をそれぞれ含めてください'
+    }
   end
 end
