50f7a7d4c6ee0dd19b88df4d17cf4474d93c0060
Nobutaka333 <taka9861+techcamp@gmail.com>
2021-03-19 15:48:47 +0900

    カテゴリープルダウン追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,25 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.order("created_at DESC")
+  end
+
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:item).permit(:name,:text,:category_id,:sale_status_id,:shipping_fee_id,:prefectures_id, :days_to_ship_id,:price,:user )
+  end
+
 end
