0921a5769ab30f8a57b5551a15244f408161e4f0
Nobutaka333 <taka9861+techcamp@gmail.com>
2021-03-21 18:05:20 +0900

    フォームから保存できなかった問題の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.new(items_params)
     if @item.save
       redirect_to root_path
     else
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
 
   private
   def items_params
-    params.require(:item).permit(:name,:text,:category_id,:sale_status_id,:shipping_fee_id,:prefectures_id, :days_to_ship_id,:price).merge(user_id: current_user.id)
+    params.permit(:image, :name, :text, :category_id, :sale_status_id, :shipping_fee_id, :prefectures_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 
 end
