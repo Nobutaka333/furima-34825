bbf5705708bd5864be6c3d10aeff257047ed00e0
Nobutaka333 <taka9861+techcamp@gmail.com>
2021-03-22 14:40:14 +0900

    修正その５　factorybotのシングルクォーテーション削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name              { Faker::Name.initials(number: 4) }
     text                 { 'test' }
-    category_id              { '1' }
-    sale_status_id { '1' }
-    shipping_fee_id             { '1' }
-    prefectures_id            { '1' }
-    days_to_ship_id       { '1' }
-    price       { '300' }
+    category_id              { 1 }
+    sale_status_id { 1 }
+    shipping_fee_id             { 1 }
+    prefectures_id            { 1 }
+    days_to_ship_id       { 1 }
+    price       { 300 }
     association :user
     
     after(:build) do |item|
